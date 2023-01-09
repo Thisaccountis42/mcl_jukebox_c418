b8f0828b0fc14cf8a07a4e5b3a40dbdf15321b1f
Thisaccountis42 <59899900+Thisaccountis42@users.noreply.github.com>
2023-01-09 14:01:44 +1300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+This is a mod (created by Thisaccountis42) that adds 4 new music discs to the game: "minimal," "depado," "krank," and "phlips," all of which were produced by C418 and released under CC-By 3.0 licenses.
+
+This mod requires Mineclone 2, more specifically mcl_jukebox, to run. It will likely also work with Mineclone 5, but this hasn't been tested.
+
+See LEGAL.txt for credits/attributions, as well as the licenses the code and media are published under.
+
+See LICENSE.txt for the GNU General Public License Version 3, for which the code of this mod is published under.
---
+++ init.lua
@@ -0,0 +1,10 @@
+mcl_jukebox.register_record("minimal", "C418", "ward", "mcl_jukebox_record_ward.png", "mcl_jukebox_track_9")
+mcl_jukebox.register_record("krank", "C418", "stal", "mcl_jukebox_record_stal.png", "mcl_jukebox_track_10")
+mcl_jukebox.register_record("depado", "C418", "cat", "mcl_jukebox_record_cat.png", "mcl_jukebox_track_11")
+mcl_jukebox.register_record("phlips", "C418", "11", "mcl_jukebox_record_11.png", "mcl_jukebox_track_12")
+
+
+minetest.register_alias("mcl_jukebox:record_9", "mcl_jukebox:record_ward")
+minetest.register_alias("mcl_jukebox:record_10", "mcl_jukebox:record_stal")
+minetest.register_alias("mcl_jukebox:record_11", "mcl_jukebox:record_cat")
+minetest.register_alias("mcl_jukebox:record_12", "mcl_jukebox:record_11")
